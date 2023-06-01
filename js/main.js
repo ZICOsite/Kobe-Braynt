320a56f1deb7d52501d323b8e65f27ba70852eef
ZICO <ziyodahmedov23@gmail.com>
2023-06-01 10:39:11 +0500

    end

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
 const headerContentBanner = document.querySelector(".header__content-banner")
 const headerContentTitle = document.querySelector(".header__content-title")
 const headerContentName = document.querySelector(".header__content-name")
+const respectVideo = document.querySelector(".respect__video")
 
 document.addEventListener("keydown", (e) => {
     if (e.key == "z" || e.key == "я") {
         headerContentBanner.classList.toggle("active")
         headerContentTitle.classList.toggle("active")
         headerContentName.classList.toggle("active")
+    } else if (e.key == "k" || e.key == "л") {
+        respectVideo.classList.toggle("active")
     }
 })
+
+const mentalityVideo = document.querySelector(".mentality__video")
+
+function some(entries) {
+    if (entries[0].isIntersecting) {
+        entries[0].target.play()
+    }
+}
+
+let elem = new IntersectionObserver(some)
+elem.observe(mentalityVideo);
